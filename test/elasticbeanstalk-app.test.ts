e4886b6c8918ab7ba9ab27d5a7b9964c121c19e3
Quoc PHAN <phquoc25@gmail.com>
2023-09-17 16:51:19 +0700

    Initial commit

---
+++ test/elasticbeanstalk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ElasticbeanstalkApp from '../lib/elasticbeanstalk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/elasticbeanstalk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ElasticbeanstalkApp.ElasticbeanstalkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
